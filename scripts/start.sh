410d4c91c169475cb578cb8d8de4e45d03df4797
Luis Bañuelos <luiscarlos.banuelos@gmail.com>
2021-01-22 12:53:55 -0800

    Maybe show the password last.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+# sync base home with mounted home
+mv /home /home-base
+ln -s /home-persistent /home
+rsync -avx /home-base /home-persistent
+
 # setup password
 USER_PASSWORD=`pwgen -c -n -1 12`
 echo "user:$USER_PASSWORD" | chpasswd
 
 echo "[ SSH Login Credentials ]"
 echo "user: $USER_PASSWORD"
-
-# sync base home with mounted home
-mv /home /home-base
-ln -s /home-persistent /home
-rsync -avx /home-base /home-persistent
 
 # execute sshd
 supervisord -n
