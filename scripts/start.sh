2c0a3d63071a627affddcf6ec69637b6647eb88d
Luis Bañuelos <luiscarlos.banuelos@gmail.com>
2021-01-22 12:15:22 -0800

    I actually want to see the password.

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,8 +4,7 @@
 USER_PASSWORD=`pwgen -c -n -1 12`
 echo "user:$USER_PASSWORD" | chpasswd
 
-echo "┌─╢ SSH Login ╟──────┐"
-echo "│ user: peePhaex8zee │"
-echo "└────────────────────┘"
+echo "[ SSH Login Credentials ]"
+echo "user: $USER_PASSWORD"
 
 supervisord -n
