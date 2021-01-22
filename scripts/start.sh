77b7a950ed6905757eece91425963cb046dae3dc
Luis Bañuelos <luiscarlos.banuelos@gmail.com>
2021-01-22 12:08:55 -0800

    Update start script.

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,8 @@
 USER_PASSWORD=`pwgen -c -n -1 12`
 echo "user:$USER_PASSWORD" | chpasswd
 
-echo "---> SSH Login <---"
-echo "-> user: $USER_PASSWORD"
+echo "┌─╢ SSH Login ╟──────┐"
+echo "│ user: peePhaex8zee │"
+echo "└────────────────────┘"
 
 supervisord -n
