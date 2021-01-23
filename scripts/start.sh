cfd060bbb90147c4e9386b473c9bdb235b1ac4b1
Luis Bañuelos <luiscarlos.banuelos@gmail.com>
2021-01-22 16:17:28 -0800

    Make docker accessible without sudo.

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,6 +10,10 @@ mount --bind /home-persistent /home
 echo "Syncing with /home-base..."
 rsync -qavx /home-base/ /home-persistent/
 
+# add to docker group
+groupadd -g $(stat -c '%g' /var/run/docker.sock) docker
+usermod -aG docker user
+
 # setup password
 USER_PASSWORD=`pwgen -c -n -1 12`
 echo "user:$USER_PASSWORD" | chpasswd
